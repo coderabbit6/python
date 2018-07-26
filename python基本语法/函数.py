9581b21cd844749ab15f32630e0e0f3e256be58f
coderabbit6 <2416841692@qq.com>
2018-07-26 22:06:26 +0800

    hanshu

--- python基本语法/函数.py
+++ python基本语法/函数.py
@@ -1,4 +1,5 @@
 #函数定义
+'''
 def shuchu():
 
     print("我在输出")
@@ -6,17 +7,13 @@ def shuchu():
 shuchu()
 
 #参数
-def hello(person,me) :
-
+def hello(person,me):
     print("hello,{0},i am {1}".format(person, me))
-    
+
 hello("zhangsan","libai")
 
 #返回值
-##无论有无返回值都用return
-##无返回值时return None
 def bijiao(a):
-
     if a>250:
         return "我大"
     elif a<250:
@@ -24,27 +21,32 @@ def bijiao(a):
     else :
         return "你是250"
 print(bijiao(300))
-
 print(bijiao(250))
-
 print(bijiao(100))
 
 #help
 help(print)
-
 help(bijiao)
+
 #九九乘法表
 for r in range(1,10):
-
     for c in range(1,r+1):
-    
         print("r*c={0}".format(r*c),end="  ")
     print("")
-    
+'''
 #函数文档
-'''
+def wendang(a):
+    '''
+    这是文档
+    i will help you
+    :param a: name
+    :return: None
+    '''
+    print("haha:{0}".format(a))
+help(wendang)
+print(wendang.__doc__)
 
-这是文档
 
-'''
+
+
 
