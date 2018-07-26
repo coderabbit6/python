bedc29de1c854555e85c1736edea5b237a87d697
coderabbit6 <2416841692@qq.com>
2018-07-26 20:19:17 +0800

    add if

---
+++ python基本语法/if语句.py
@@ -0,0 +1,8 @@
+gender = input("please input your sex :")
+print("what your put is :{0}".format(gender))
+if gender == "man":
+    print("you are a man")
+elif gender == "woman":
+    print("you are a girl")
+else:
+    print("hehe")
