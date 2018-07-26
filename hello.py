fdff35855b214030eaa69f086a00b2e108236c5e
coderabbit6 <2416841692@qq.com>
2018-07-26 11:28:58 +0800

    meisha

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello,world")
+name = "zhang san feng"
+print(name.upper())
+print(name.lower())
+print(name.title())
+a = [1, 2, 3, 4, 5]
+for i in a:
+    print(i, end="  ")
+a.append(10)
+print(a)
+print(a(-1))
