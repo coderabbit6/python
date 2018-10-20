9969c88cc7fa2b3ca25be5d41c338538e8fe450e
coderabbit6 <2416841692@qq.com>
2018-10-20 20:25:12 +0800

    data analytics

---
+++ Python Data Analytics/NumPy/ndarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# a = np.array([1,2,3])
+# print(a*2)
+# print(a)
+# print(type(a))
+# print(a.ndim)
+# print(a.size)
+# print(a.shape)
+
+######创建数组
+c = np.array([[1,2,3],[4,5,6]])
+print(c)
